9a874a54ed6e74561b3158c271f751e0d8e118bb
栾添昊 <Lancelot@luantianhaodeMacBook-Pro.local>
2018-12-16 23:13:29 +0800

    changed three.py

--- three.py
+++ three.py
@@ -1,33 +1,14 @@
 import numpy as np
+import fileinput, re
 
-f = open('three.data','r')
-line = f.readline()
+# part one
 matrix = np.zeros((1050, 1050), dtype=int)
+for line in fileinput.input('three.data'):
+    id, x, y, w, h = map(int, re.findall(r'\d+', line))
+    matrix[y : y + h, x : x + w] += 1
+print(len(np.extract(matrix > 1, matrix)))
 
-while line:
-    claim = line.strip()
-    pos1 = claim.find('@')
-    pos2 = claim.find(':')
-    left, top = claim[pos1+2:pos2].split(',')
-    width, height = claim[pos2+2:].split('x')
-    left, top, width, height= int(left), int(top), int(width), int(height)
-    matrix[top:top+height, left:left+width] += 1
-    line = f.readline()
-
-condition = matrix > 1
-num = len(np.extract(condition, matrix))
-print(num)
-
-f = open('three.data','r')
-line = f.readline()
-
-while line:
-    claim = line.strip()
-    pos1 = claim.find('@')
-    pos2 = claim.find(':')
-    left, top = claim[pos1+2:pos2].split(',')
-    width, height = claim[pos2+2:].split('x')
-    left, top, width, height= int(left), int(top), int(width), int(height)
-    if (matrix[top:top+height, left:left+width] == 1).all():
-        print(claim)
-    line = f.readline()
+# part two
+for line in fileinput.input('three.data'):
+    id, x, y, w, h = map(int, re.findall(r'\d+', line))
+    if (matrix[y : y + h, x : x + w] == 1).all(): print(id)
